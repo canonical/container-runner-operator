1d105a9c8df21078fc69fbea6817a0ab27272bf8
Matthew Hagemann <matt.hagemann@canonical.com>
2024-09-04 15:51:49 +0200

    test: integration tests for ratings service

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -3,8 +3,13 @@
 # See LICENSE file for licensing details.
 
 import asyncio
+import os
+import secrets
 import time
 
+import grpc
+import ratings_api.ratings_features_user_pb2 as pb2
+import ratings_api.ratings_features_user_pb2_grpc as pb2_grpc
 import requests
 from pytest import mark
 from pytest_operator.plugin import OpsTest
@@ -13,15 +18,29 @@ CONTAINER_RUNNER = "container-runner"
 UNIT_0 = f"{CONTAINER_RUNNER}/0"
 DB = "db"
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", ".."))
+
 
 @mark.abort_on_fail
 @mark.skip_if_deployed
 async def test_deploy(ops_test: OpsTest, container_runner_charm):
-    await ops_test.model.deploy(await container_runner_charm, application_name=CONTAINER_RUNNER)
+    config = {
+        "container-image-uri": "ghcr.io/ubuntu/app-center-ratings:sha-7f05d08",
+        "host-port": 81,
+        "container-port": 81,
+        "database-expected": True,
+    }
+    resources = {"env-file": f"{BASE_DIR}/tests/integration/env-vars.env"}
+    await ops_test.model.deploy(
+        await container_runner_charm,
+        application_name=CONTAINER_RUNNER,
+        config=config,
+        resources=resources,
+    )
     # issuing dummy update_status just to trigger an event
     async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(apps=[CONTAINER_RUNNER], status="active", timeout=1000)
-        assert ops_test.model.applications[CONTAINER_RUNNER].units[0].workload_status == "active"
+        await ops_test.model.wait_for_idle(apps=[CONTAINER_RUNNER], status="waiting", timeout=1000)
+        assert ops_test.model.applications[CONTAINER_RUNNER].units[0].workload_status == "waiting"
 
 
 @mark.abort_on_fail
@@ -50,10 +69,14 @@ async def test_hello_world_image(ops_test: OpsTest):
     print(f"Connecting to address: {status}")
     address = status["applications"][CONTAINER_RUNNER]["units"][unit]["public-address"]
     print(f"Connecting to address: {address}")
-    connection_string = f"http://{address}:80"
+    connection_string = f"{address}:81"
 
-    response = requests.get(connection_string)
-    assert "Hello World" in response.text
+    channel = grpc.insecure_channel(connection_string)
+    stub = pb2_grpc.UserStub(channel)
+    message = pb2.AuthenticateRequest(id=secrets.token_hex(32))
+    print(f"Message sent: {message}")
+    response = stub.Authenticate(message)
+    assert response.token
 
 
 @mark.abort_on_fail
@@ -64,10 +87,11 @@ async def test_config_update(ops_test: OpsTest):
             "host-port": "5678",
             "container-port": "5678",
             "container-image-uri": "hashicorp/http-echo",
+            "database-expected": "false"
         }
     )
     await ops_test.model.wait_for_idle(
-        apps=[DB], status="active", raise_on_blocked=True, timeout=1000
+        apps=[CONTAINER_RUNNER], status="active", raise_on_blocked=True, timeout=1000
     )
     time.sleep(10)
     status = await ops_test.model.get_status()  # noqa: F821
